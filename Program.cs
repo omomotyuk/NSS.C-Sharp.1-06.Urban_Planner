c7bb12b579d6e0ce0fbedc9cbd876b5aa481c63b
Alex Momotyuk <o.a.momotyuk@gmail.com>
2019-11-18 15:40:51 -0600

    Final version of the code

--- Program.cs
+++ Program.cs
@@ -9,42 +9,27 @@ namespace _1_06.Urban_Planner
         {
             List<Building> buildings = new List<Building>();
 
-            Building FiveOneTwoEigth = new Building("512 8th Avenue");
-            // Give each building a width, height, and number of stories
-            FiveOneTwoEigth.Width = 10_0;
-            FiveOneTwoEigth.Depth = 10_0;
-            FiveOneTwoEigth.Stories = 1;
-            // Construct each building
-            FiveOneTwoEigth.Construct();
-            // Have business people in your city purchase each of your buildings
-            FiveOneTwoEigth.Purchase("First Owner");
-
-            buildings.Add(FiveOneTwoEigth);
-
-            //
-            Building OneZeroOneSix = new Building("101 6th Avenue");
-            OneZeroOneSix.Width = 11_0;
-            OneZeroOneSix.Depth = 11_0;
-            OneZeroOneSix.Stories = 2;
-            OneZeroOneSix.Construct();
-            OneZeroOneSix.Purchase("Second Owner");
-
-            buildings.Add(OneZeroOneSix);
-
-            //
-            Building TreeZeroFiveNine = new Building("305 9th Avenue");
-            TreeZeroFiveNine.Width = 12_0;
-            TreeZeroFiveNine.Depth = 12_0;
-            TreeZeroFiveNine.Stories = 3;
-            TreeZeroFiveNine.Construct();
-            TreeZeroFiveNine.Purchase("Third Owner");
-
-            buildings.Add(TreeZeroFiveNine);
+            buildings.Add( newBuilding("512 8th Avenue", 10_0, 10_0, 1, "First Owner") );
+            buildings.Add( newBuilding("101 6th Avenue", 11_0, 11_0, 2, "Second Owner") );
+            buildings.Add( newBuilding("305 9th Avenue", 12_0, 12_0, 3, "Third Owner") );
 
             foreach( Building building in buildings )
             {
                 building.PrintInfo();
             }
+        }
+
+        static Building newBuilding( string address, double width, double depth, int stories, string owner )
+        {
+            Building building = new Building(address);
+
+            building.Width = width;
+            building.Depth = depth;
+            building.Stories = stories;
+            building.Construct();
+            building.Purchase(owner);
+
+            return building;
         }
     }
 }
